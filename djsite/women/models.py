282d4989bc1fd0bc86714ea19169cb95377af698
nika-doroshkevich <veronika_doroshkevich@mail.ru>
2023-12-06 00:08:02 +0300

    Added: Pagination

--- djsite/women/models.py
+++ djsite/women/models.py
@@ -21,7 +21,7 @@ class Women(models.Model):
     class Meta:
         verbose_name = 'Famous women'
         verbose_name_plural = 'Famous women'
-        ordering = ['-time_create', 'title']
+        ordering = ['id']
 
 
 class Category(models.Model):
--- djsite/women/utils.py
+++ djsite/women/utils.py
@@ -10,6 +10,8 @@ menu = [{'title': "About site", 'url_name': 'about'},
 
 
 class DataMixin:
+    paginate_by = 3
+
     def get_user_context(self, **kwargs):
         context = kwargs
         categories = Category.objects.annotate(Count('women'))
