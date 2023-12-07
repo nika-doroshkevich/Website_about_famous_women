aa789ab602a37ed4c299c8b3b800e802ee15ae61
nika-doroshkevich <veronika_doroshkevich@mail.ru>
2023-12-07 04:20:48 +0300

    Added: Authorization

--- djsite/women/views.py
+++ djsite/women/views.py
@@ -1,10 +1,12 @@
+from django.contrib.auth import logout
+from django.contrib.auth.views import LoginView
 from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .forms import AddPostForm, RegisterUserForm
+from .forms import AddPostForm, RegisterUserForm, LoginUserForm
 from .utils import *
 
 menu = [{'title': "About site", 'url_name': 'about'},
@@ -49,10 +51,6 @@ def contact(request):
     return HttpResponse("Feedback")
 
 
-def login(request):
-    return HttpResponse("Login")
-
-
 def page_not_found(request, exception):
     return HttpResponseNotFound("<h1>Page not found</h1>")
 
@@ -94,3 +92,21 @@ class RegisterUser(DataMixin, CreateView):
         context = super().get_context_data(**kwargs)
         c_def = self.get_user_context(title="Registration")
         return dict(list(context.items()) + list(c_def.items()))
+
+
+class LoginUser(DataMixin, LoginView):
+    form_class = LoginUserForm
+    template_name = 'women/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Authorization")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
